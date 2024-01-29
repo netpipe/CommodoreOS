2af4fb5befa68a63ef30e6bbc74dbd8648fc9894
netpipe <teslaenergy@gmail.com>
2024-01-28 17:50:24 -0700

    testing

--- Encryption/rc4.c
+++ Encryption/rc4.c
@@ -1,25 +1,31 @@
 #include <stdint.h>
+#include <string.h>
 
 void rc4_init(uint8_t* s, const uint8_t* key, size_t key_len) {
-    for (int i = 0; i < 256; i++) {
+    int i;
+    uint8_t j = 0;
+    uint8_t temp;
+
+    for (i = 0; i < 256; i++) {
         s[i] = i;
     }
 
-    uint8_t j = 0;
-    for (int i = 0; i < 256; i++) {
+    for (i = 0; i < 256; i++) {
         j += s[i] + key[i % key_len];
-        uint8_t temp = s[i];
+        temp = s[i];
         s[i] = s[j];
         s[j] = temp;
     }
 }
 
 void rc4_crypt(uint8_t* s, uint8_t* data, size_t data_len) {
-    uint8_t i = 0, j = 0;
-    for (size_t k = 0; k < data_len; k++) {
+    uint8_t i = 0, j = 0, temp;
+    size_t k;
+
+    for (k = 0; k < data_len; k++) {
         i++;
         j += s[i];
-        uint8_t temp = s[i];
+        temp = s[i];
         s[i] = s[j];
         s[j] = temp;
         data[k] ^= s[(s[i] + s[j]) & 0xFF];
@@ -30,13 +36,6 @@ int main() {
     uint8_t s[256];
     uint8_t key[] = {0x12, 0x34, 0x56, 0x78}; // Key for RC4
     uint8_t data[] = "Hello World"; // Data to be encrypted
-
-    rc4_init(s, key, sizeof(key));
-    rc4_crypt(s, data, strlen(data)); // Encrypt
-
-    rc4_init(s, key, sizeof(key)); // Reinitialize for decryption
-    rc4_crypt(s, data, strlen(data)); // Decrypt
-
-    return 0;
-}
+    size_t data_len = strlen(data);
+    size_t key_len = s
 
--- Encryption/xor.c
+++ Encryption/xor.c
@@ -2,7 +2,8 @@
 #include <string.h>
 
 void xor_cipher(uint8_t* data, size_t data_len, const uint8_t* key, size_t key_len) {
-    for (size_t i = 0; i < data_len; ++i) {
+    size_t i;
+    for (i = 0; i < data_len; ++i) {
         data[i] ^= key[i % key_len];
     }
 }
@@ -10,9 +11,11 @@ void xor_cipher(uint8_t* data, size_t data_len, const uint8_t* key, size_t key_l
 int main() {
     uint8_t data[] = "Hello World"; // Data to be encrypted
     uint8_t key[] = {0x12, 0x34, 0x56, 0x78}; // Encryption key
+    size_t data_len = strlen(data);
+    size_t key_len = sizeof(key);
 
-    xor_cipher(data, strlen(data), key, sizeof(key)); // Encrypt
-    xor_cipher(data, strlen(data), key, sizeof(key)); // Decrypt (same as encryption)
+    xor_cipher(data, data_len, key, key_len); // Encrypt
+    xor_cipher(data, data_len, key, key_len); // Decrypt (same as encryption)
 
     return 0;
 }
