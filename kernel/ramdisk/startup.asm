0be58bf90cf94d21d2a74a06fa8a6dbe5cae2012
netpipe <teslaenergy@gmail.com>
2024-01-28 21:48:08 -0700

    testing

--- kernel/ramdisk/startup.asm
+++ kernel/ramdisk/startup.asm
@@ -1,24 +1,39 @@
 ; Include the C64 system constants
 .include "c64.inc"
 
-.org $0801       ; Set the assembly address to $0801
+.org $0801
     .byte $0c, $08 ; SYS $080C
 
-.org $0803       ; Set the assembly address to $0803
+.org $0803
 main:
-    ; Your minimal initialization code here
-
-    ; Example: Clear the screen
+    ; Clear the screen
     LDA #$00
     STA $0400
     LDA #$12
     STA $D020
 
-    ; Call the main C function
-    JMP main
+    ; Set screen memory address
+    LDA #<SCREEN_ADDRESS
+    STA $07
+    LDA #>SCREEN_ADDRESS
+    STA $08
 
-; Define the screen memory location
+    ; Print "Hello, World!" to the screen
+    LDX #0
+loop:
+    LDA MESSAGE, X
+    BEQ done
+    STA $0400, X
+    INX
+    JMP loop
+done:
+    ; Wait for key press
+    JSR $FFE4
+
+    ; Return to BASIC
+    RTS
+
 SCREEN_ADDRESS = $0400
-
-; End of file
+MESSAGE:
+    .text "Hello, World!"
 
